e0c4948079b34bb81d13f2dea1e37b47b8998fae
Jarrod <jarrod@span-eng.com>
2025-12-16 11:26:39 +1000

    updated config again:

--- src/sia_test_bench/app_config.py
+++ src/sia_test_bench/app_config.py
@@ -6,6 +6,8 @@ from pydoover import config
 class SiaTestBenchConfig(config.Schema):
     def __init__(self):
         self.pump_controller = config.Application("Pump Controller App", description="The pump 1 application")
+        self.tank_level_app = config.Application("Tank Level App", description="The tank level application")
+        self.pressure_sensor_app = config.Application("Pressure App", description="The pressure sensor application")
         # self.barcode_reader_path = config.String("Barcode Reader Path")
         
 
